a608c332feeffa2daba8d1db1aa2529fa74049fb
韩国星 <hanguoxing@meizu.com>
2017-03-23 11:37:27 +0800

    frist commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # browser_cookie
-A simple, lightweight JavaScript API for handling browser cookies
+*A simple, lightweight JavaScript API for handling browser cookies*
+
+## Build Status Matrix
+![Selenium Test Status](https://camo.githubusercontent.com/1176ea6ce99215a0b8de53defaa91c77669b1921/68747470733a2f2f73617563656c6162732e636f6d2f62726f777365722d6d61747269782f6a71756572792d636f6f6b69652e737667)]
+
+## Getting started
+
+Download cookie.js file and include it in your HTML document, this will add a global object called `BrowserCookie`:
+
+```
+<script src="cookie.js"></script>
+```
+Or, you can 
+
+```
+var BrowserCookie = require('cookie.js');
+```
+## Write a cookie
+
+A cookie is set with a simple object as the first parameter:
+
+```
+BrowserCookie.cookie("CookieKey", CookieValue, {expires: Date});
+```
+The `Date` demo:
+
+```
+var curDate = new Date();
+curDate.setTime(curDate.getTime() + (1 * 60 * 1000));//set 1 minute expire
+```
+
+## Read a cookie
+
+```
+BrowserCookie.cookie("CookieKey")
+```
+## Delete a cookie
+
+```
+BrowserCookie.deleteCookie("CookieKey")
+```
+
+## Authors
+
+[Hancoson](https://github.com/Hancoson)
---
+++ js/cookie.js
@@ -0,0 +1,81 @@
+/**
+ * BrowserCookie Plugin v1.0.0
+ * https://github.com/Hancoson/browser_cookie
+ * 
+ * Created by Guoxing.han on 2017-03-23.
+ * Released under the MIT license
+ */
+
+var BrowserCookie = (function () {
+
+  var decode = function (value) {
+    return decodeURIComponent(value);
+  },
+    encode = function (value) {
+      return encodeURIComponent(value);
+    };
+
+  return {
+    /**
+     * @param {key} 
+     * @param {value} 
+     * @param {option} set the `expires`.
+     */
+    cookie: function (key, value, option) {
+      var t, days, items, cookies,
+        i, len,
+        item,
+        result;
+
+      // if the arguments' length  greater than 1, it's set the cookie
+      if (arguments.length > 1) {
+        option = option || {};
+
+        if (typeof option.expires === "number") {
+          days = option.expires;
+          t = option.expires = new Date();
+          t.setTime(+t + days * 864e+5);// 86400000MS ; +t, Object to Number;
+        }
+        items = [
+          encode(key), "=", encode(value),
+          option.expires ? "; expires=" + option.expires.toUTCString() : '',
+          option.path ? "; path=" + path : '',
+          option.domain ? "; domain=" + domain : '',
+          option.secure ? "; secure" : ''
+        ].join('');
+
+        return document.cookie = items;
+      }
+      //
+      cookies = document.cookie ? document.cookie.split("; ") : [];
+      for (i = 0, len = cookies.length; i < len; i++) {
+        item = cookies[i].split("=");
+        if (key && key == decode(item[0])) {
+          result = decode(item[1]);
+          break;
+        }
+      }
+      return result;
+    },
+    deleteCookie: function (key) {
+      if (this.cookie(key) === undefined) {
+        return false;
+      }
+
+      this.cookie(key, "", { expires: -1 });
+
+      return !this.cookie(key);
+    }
+  }
+
+})();
+
+// to  modularization
+if (typeof exports !== 'undefined') {
+  if (typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = BrowserCookie;
+  }
+  exports.BrowserCookie = BrowserCookie;
+} else {
+  window.BrowserCookie = BrowserCookie;
+}
